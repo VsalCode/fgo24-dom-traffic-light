5f922a9a141d5f7755ef84c2f376ec4d2a8ae0e9
VsalCode <vsal.ilham@gmail.com>
2025-05-07 20:33:44 +0700

    fix: traffic lights active set timeout

--- scripts.js
+++ scripts.js
@@ -2,15 +2,36 @@ const red = document.querySelector(".kotak:nth-child(1) > div");
 const yellow = document.querySelector(".kotak:nth-child(2) > div");
 const green = document.querySelector(".kotak:nth-child(3) > div");
 
-setInterval(() => {
+const trafficLights = () => {
   red.classList.add("red");
-  setInterval(() => {
+  setTimeout(() => {
+    red.classList.remove("red");
     yellow.classList.add("yellow");
-    red.remove("red");
-    setInterval(() => {
+    setTimeout(() => {
+      yellow.classList.remove("yellow");
       green.classList.add("green");
-      red.remove("red");
-      yellow.remove("red");
-    }, 3000);
-  }, 2000);
-});
+      setTimeout(() => {
+        green.classList.remove("green");
+        trafficLights(); 
+      }, 3000); 
+    }, 2000); 
+  }, 3000); 
+};
+
+trafficLights();
+
+
+  // setTimeout(() => {
+  //   red.classList.add("red");
+
+  //   setTimeout(() => {
+  //     yellow.classList.add("yellow");
+  //     red.remove("red");
+  //     setTimeout(() => {
+  //       yellow.remove("red");
+  //       red.remove("red");
+  //       green.classList.add("green");
+  //       window.location.reload()
+  //     }, 3000);
+  //   }, 2000);
+  // }, 3000);
